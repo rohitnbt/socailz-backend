8a91e2416ba4f83fd3261cc386737e4d51f8308e
Rohit Singh <153371016+rohitnbt@users.noreply.github.com>
2024-04-03 11:08:50 +0000

    mongodb

--- index.js
+++ index.js
@@ -1,41 +1,53 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors'
-import multer from 'multer';
-import path from 'path';
-import { PORT } from './config.js';
-import UserRoute from './db/routes/UsersRoute.js'
-import bodyParser from 'body-parser';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import multer from "multer";
+import path from "path";
+import { PORT } from "./config.js";
+import UserRoute from "./db/routes/UsersRoute.js";
+import bodyParser from "body-parser";
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'public/images')
-    },
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
 
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + '_' + Date.now() + path.extname(file.originalname))
-    }
-})
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname + "_" + Date.now() + path.extname(file.originalname),
+    );
+  },
+});
 
 const upload = multer({
-    storage: storage
-})
+  storage: storage,
+});
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use('/', upload.fields([{ name: 'profilePicture', maxCount: 1 }, { name: 'bannerImage', maxCount: 1 }]), UserRoute);
+app.use(
+  "/",
+  upload.fields([
+    { name: "profilePicture", maxCount: 1 },
+    { name: "bannerImage", maxCount: 1 },
+  ]),
+  UserRoute,
+);
 
-mongoose.connect('mongodb://localhost:27017/Socialz')
-.then(() => {
-    console.log('Database is connected')
-})
+mongoose
+  .connect(
+    "mongodb+srv://root:root@booksstore.clnmiai.mongodb.net/socialz?retryWrites=true&w=majority&appName=BooksStore",
+  )
+  .then(() => {
+    console.log("Database is connected");
+  });
 
-
-app.listen(PORT, ()=> {
-    console.log(`App is running at port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`App is running at port ${PORT}`);
+});
